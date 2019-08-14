da52f6e77d9a428aa06fbfc5071d070ddd810d6b
NHanzhuha@healthprecision.com <nhanzhuha@healthprecision.com>
2019-08-14 18:21:50 +0300

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {MenuItem} from 'primeng/api';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'MultyForm';
-  items: MenuItem[];
-  activeIndex = 0;
   constructor() {}
-  ngOnInit() {
-    this.items = [
-      {label: 'Full Name'},
-      {label: 'Address'},
-      {label: 'Phone Number'}
-    ];
 
-  }
 }
